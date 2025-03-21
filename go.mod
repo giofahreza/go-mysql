cb676fd734c738f6164dd434f67e5f80ba3b5793
Giofahreza <giofahreza2000@gmail.com>
2025-03-21 21:54:20 +0700

    Update main.go, adding published module example

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.23.3
 
 require (
 	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/giofahreza/go-calculate v0.0.4 // indirect
 	github.com/go-sql-driver/mysql v1.9.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"go-mysql/cli"
 	"go-mysql/config"
+
+	c "github.com/giofahreza/go-calculate"
 )
 
 func main() {
 	config.InitDB()
+	fmt.Println("Hasil module calculate : ", c.Div(8, 2))
 	cli.Run()
 }
